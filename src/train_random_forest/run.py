48b26379a079161316b881297c7d84cfb41dfdec
lilei2626 <48934047+lilei2626@users.noreply.github.com>
2025-08-16 02:52:37 -0500

    Update run.py

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -21,7 +21,7 @@ from sklearn.preprocessing import OrdinalEncoder, FunctionTransformer, OneHotEnc
 
 import wandb
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_error, r2_score
 from sklearn.pipeline import Pipeline, make_pipeline
 
 
@@ -93,6 +93,7 @@ def go(args):
     r_squared = sk_pipe.score(X_val, y_val)
 
     y_pred = sk_pipe.predict(X_val)
+    r_squared = r2_score(y_val, y_pred)
     mae = mean_absolute_error(y_val, y_pred)
 
     logger.info(f"Score: {r_squared}")
