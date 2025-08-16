abebff9cadf8c5d782f2339995b3e0933f514fce
lilei2626 <48934047+lilei2626@users.noreply.github.com>
2025-08-16 01:14:54 -0500

    Update run.py
    Added 5 parts for non-ordinal, build inference, fit pipeline, save pipeline, and log metrics to W&B

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -73,6 +73,8 @@ def go(args):
 
     ######################################
     # Fit the pipeline sk_pipe by calling the .fit method on X_train and y_train
+    sk_pipe.fit(X_train, y_train)
+
     # YOUR CODE HERE
     ######################################
 
@@ -97,6 +99,8 @@ def go(args):
     # HINT: use mlflow.sklearn.save_model
     mlflow.sklearn.save_model(
         # YOUR CODE HERE
+        sk_pipe,
+        path="random_forest_dir",
         input_example = X_train.iloc[:5]
     )
     ######################################
@@ -119,6 +123,7 @@ def go(args):
     # Here we save variable r_squared under the "r2" key
     run.summary['r2'] = r_squared
     # Now save the variable mae under the key "mae".
+    run.summary['mae'] = mae
     # YOUR CODE HERE
     ######################################
 
@@ -163,6 +168,8 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # 2 - A OneHotEncoder() step to encode the variable
     non_ordinal_categorical_preproc = make_pipeline(
         # YOUR CODE HERE
+        SimpleImputer(strategy="most_frequent"),
+        OneHotEncoder(handle_unknown="ignore")
     )
     ######################################
 
@@ -226,6 +233,8 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     sk_pipe = Pipeline(
         steps =[
         # YOUR CODE HERE
+            ("preprocessor", preprocessor),
+            ("random_forest", random_forest),
         ]
     )
 
